56cd723cc81a793ae025b78d7d2be4ddef58eed5
cordelia <cordelia@example>
2022-03-30 23:27:56 +0900

    Add 500 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,18 @@ class ApplicationController < ActionController::Base
   # リクエストされたURLによって使うview layoutを変更する
   layout :set_layout
 
+  # 例外補足
+  rescue_from StandardError, with: :rescue500
+
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
       Regexp.last_match[1]
     else
       "customer"
     end
+  end
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
   end
 end
